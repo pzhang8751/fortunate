64b81b5025781340e951b6c2c35248a910383818
Patrick Zhang <patrick8751@gmail.com>
2026-02-18 16:41:26 -0500

    main page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,19 @@
 
-
 function App() {
-
   return (
     <>
-      <p>Hello this is the beginning of the app</p>
+      <section className="bg-[url(/flowers.png)] h-[95vh] flex flex-col justify-end text-white px-5 lg:px-8 pt-5 pb-[20vh] lg:pb-32">
+        <h1 className="text-5xl sm:text-7xl md:text-8xl font-semibold">We, the Fortunate</h1>
+        <h2 className="mt-3 text-xl md:text-3xl">Fateful encounters create lifelong memories</h2>
+      </section>
+      <section className="mt-2 px-5 py-5 text-center">
+        <h3 className="text-2xl md:text-4xl">Inspired by a movie, a trip, and a conversation</h3>
+        <p><span className="italic">Past Lives</span>, by Celine Song, is a film that explores the concept of fate through the lens of a long-lost relationship between two lovers. Nora, a Korean immigrant, and her childhood sweetheart, Hae Sung, reunite for two days in New York City. The two first met in elementary school, establishing a close friendship before Nora's family moved to the United States.  </p>
+      </section>
+      {/* <section>
+        <h2>Behind the project</h2>
+        <p>A lot can change in a year. </p>
+      </section> */}
     </>
   )
 }
