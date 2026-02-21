8b60757d4dfdaa4faf9447156ae104fb88063ee1
Patrick Zhang <patrick8751@gmail.com>
2026-02-21 12:59:06 -0500

    home page update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,16 @@
 function App() {
   return (
     <>
-      <section className="bg-[url(/flowers.png)] h-[95vh] flex flex-col justify-end text-white px-5 lg:px-8 pt-5 pb-[20vh] lg:pb-32">
+      <section className="bg-[url(/flowers.webp)] h-[95vh] flex flex-col justify-end text-white px-5 lg:px-8 pt-5 pb-[20vh] lg:pb-32">
         <h1 className="text-5xl sm:text-7xl md:text-8xl font-semibold">We, the Fortunate</h1>
         <h2 className="mt-3 text-xl md:text-3xl">Fateful encounters create lifelong memories</h2>
       </section>
       <section className="mt-2 px-5 py-5 text-center">
         <h3 className="text-2xl md:text-4xl">Inspired by a movie, a trip, and a conversation</h3>
-        <p><span className="italic">Past Lives</span>, by Celine Song, is a film that explores the concept of fate through the lens of a long-lost relationship between two lovers. Nora, a Korean immigrant, and her childhood sweetheart, Hae Sung, reunite for two days in New York City. The two first met in elementary school, establishing a close friendship before Nora's family moved to the United States.  </p>
+        <p><span className="italic">Past Lives</span>, by Celine Song, is a film that explores the concept of fate through the lens of a long-lost relationship between two lovers. Nora, a Korean immigrant, and her childhood sweetheart, Hae Sung, have a complicated timeline. Before Nora's family moved from Korea to the United States, the two first met in elementary school as close friends, then reconnecting over Skype as young adults and lovers. However, their relationship would eventually turn sour with conflicts arising from the distance and lack of commitment.</p>
+        <p>A decade later, the two reconnect in New York City, but at very different times in their lives. Nora is now married to another man, Arthur, and Hae Sung recently broke up with his girlfriend. </p>
+        <p>There comes a scene, where Nora, Arthur, and Hae Sung are sitting at a table talking with each other. The conversation and questions between them ask the audience to consider life's what-ifs. What if Nora and Hae Sung's love had worked out earlier? What if Nora had never met Arthur after Hae Sung? These seemingly undestined moments in the grand scheme of life have a profound impact on the paths we take and where we ultimately end up.</p>
+        <p>At the heart of the film is the Korean word 인연 (Inyeon)&mdash;referring to the Budhhist concept that our relationships in this lifetime are intricately intertwined from all our past lives. From </p>
       </section>
       {/* <section>
         <h2>Behind the project</h2>
