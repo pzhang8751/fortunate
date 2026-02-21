d926c67ac79750196908ade2c33e369077f09bad
Patrick Zhang <patrick8751@gmail.com>
2026-02-21 12:04:21 -0500

    image change

--- frontend/src/pages/WritePage.jsx
+++ frontend/src/pages/WritePage.jsx
@@ -3,7 +3,7 @@ import EntryForm from "../components/EntryForm";
 export default function WritePage() {
     return (
         <>
-            <section className="bg-[url(/flowers.png)] h-[20vh] flex flex-col justify-end text-white px-5 lg:px-8 py-5">
+            <section className="bg-[url(/flowers.webp)] h-[20vh] flex flex-col justify-end text-white px-5 lg:px-8 py-5">
                 <h1 className="font-semibold text-2xl lg:text-5xl">Leave an Entry</h1>
             </section>
             <section className="px-5 py-5 flex flex-col place-items-center">
